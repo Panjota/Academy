a25092d06bff22ca1a6553670e71182ec4336a8c
Alexandro Pantoja <93011105+Panjota@users.noreply.github.com>
2022-08-11 13:42:24 -0400

    Add files via upload

---
+++ Euclides.py
@@ -0,0 +1,18 @@
+#Euclides
+
+m=int(input("m: "))
+n=int(input("n: "))
+m = m % n
+u = m
+    
+while True:
+    if u == 0:
+        print(n)
+        break
+    else:
+        m = n
+        n = u
+        
+        m = m % n
+        u = m
+        
